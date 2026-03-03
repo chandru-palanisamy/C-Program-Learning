116da565604c9402be8b0bc47891681b104ad99b
CHANDRU P <chandru444king@gmail.com>
2026-03-03 21:35:08 +0530

    3/3/26

--- leap66.c
+++ leap66.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
 void main()
 {
-    int i,n,flag=0;
+    int i,j,n;
     printf("enter a number:");
     scanf("%d",&n);
-    for(i=2;i<n;i++)
-    {
-        if(n%i!=0)
-        continue;
-    
-    else
-    {
-        flag=1;
-        printf("not a prime\n");
-        break;
-    }
-}
-if(flag==0)
+     
+    for(i=1;i<=n;i++)
 {
-    printf("prime");
-
+    for(j=1;j<=i;j++)
+    {
+        printf("%d,j");
+    }
+    printf("\n");
 }
 
 }

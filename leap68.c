a88d3a42767c2845a1fe51c18d5c4098af3a5d9c
CHANDRU P <chandru444king@gmail.com>
2026-03-09 21:27:56 +0530

    9\3\26

---
+++ leap68.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void main()
+{
+    int i,j,n,flag=0;
+    printf("enter a number:");
+    scanf("%d",&n);
+    for(j=1;j<=n;j++ )
+    {
+        if(j==1)
+        {
+        printf("not prime\n");
+        continue;
+    }
+    flag=0;
+    for(i=2;i<j;i++)
+    {
+    if(j%i!=0)
+    {
+        continue;
+    }
+    else{
+       flag=1;
+       printf("not prime \n");
+       break;
+    }
+}
+
+if (flag==0)
+{
+printf("prime\n");
+
+}
+    }
+    
+}
+
+
+
